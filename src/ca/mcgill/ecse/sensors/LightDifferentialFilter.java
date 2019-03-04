2188dca5e9962dd8a6ad0fb0c10645dab9258870
alicekazarine26 <46795081+alicekazarine26@users.noreply.github.com>
2019-03-04 10:09:16 -0500

    Light and US sensor filters copied from Lab5

--- src/ca/mcgill/ecse/sensors/LightDifferentialFilter.java
+++ src/ca/mcgill/ecse/sensors/LightDifferentialFilter.java
@@ -1,5 +1,43 @@
 package ca.mcgill.ecse.sensors;
 
-public class LightDifferentialFilter {
+import lejos.robotics.SampleProvider;
+
+public class LightDifferentialFilter extends Thread {
+
+
+    private float pastSample;
+    private SampleProvider colorProvider;
+    private float[] sampleLSData;
+
+    public LightDifferentialFilter(SampleProvider LSprovider, float[] sampleLS) {
+        this.pastSample = 0;
+        this.colorProvider = LSprovider;
+        this.sampleLSData = sampleLS;
+        getDeltaL(); // get an initial intensity
+    }
+    
+    /**
+     * Fetches new samples to get rid of old ones.
+     */
+    public void flush() {
+        getDeltaL();
+    }
+
+    /**
+     * 
+     * @return the difference between two sequential light sensor sample polls.
+     */
+    public float getDeltaL() {
+        
+        colorProvider.fetchSample(sampleLSData, 0);
+
+        // calculate the difference between current and past light intensity
+        float deltaL = (100 * sampleLSData[0] - pastSample);//TODO make it 1000 * ...
+
+        // store the last data in past Data
+        pastSample = 100 * sampleLSData[0];
+
+        return deltaL;
+    }
 
 }
