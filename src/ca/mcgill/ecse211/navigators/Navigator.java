531bea80c73dfbf18b8a1e3d3212ba2fc240e6af
alicekazarine26 <46795081+alicekazarine26@users.noreply.github.com>
2019-03-05 17:54:52 -0500

    parameters passed by array now

--- src/ca/mcgill/ecse211/navigators/Navigator.java
+++ src/ca/mcgill/ecse211/navigators/Navigator.java
@@ -1,4 +1,6 @@
 package ca.mcgill.ecse211.navigators;
+import java.util.function.IntPredicate;
+
 import ca.mcgill.ecse211.localizers.Localization;
 import ca.mcgill.ecse211.odometer.*;
 
@@ -7,23 +9,28 @@ public class Navigator {
 	private static final double TILE_SIZE = 30.48;
 	private static final double VERT_SENSOR_OFFSET = 5; 
 	
-	private static MovementController move;
+	private static MovementController movementController;
 	private static Odometer odo;
 	private static Localization localizer;
-	private static int TLLX, TLLY, TURX, TURY;
+	
+	private static int[] TLL = new int[2];
+	private static int[] TUR = new int[2];
+	
+	
+	
 	private static int bridgeTileLength;
 	private static int SC;
 
-	public Navigator(MovementController move, Odometer odo, Localization localizer, int TLLX, int TLLY, int TURX, int TURY, int SC) {
-		this.move = move;
+	public Navigator(MovementController movementController, Odometer odo, Localization localizer, int[] TLL, int TUR[], int SC) {
+		this.movementController = movementController; 
 		this.odo = odo;
-		this.TLLX = TLLX;
-		this.TLLY = TLLY;
-		this.TURX = TURX;
-		this.TURY = TURY;
+		this.TLL[0] = TLL[0];
+		this.TLL[1] = TLL[1];
+		this.TUR[0] = TUR[0];
+		this.TUR[1] = TUR[1];
 		this.localizer = localizer;
 		this.SC = SC;
-		bridgeTileLength = (Math.abs(TLLX-TURX) > Math.abs(TLLY-TURY)) ? Math.abs(TLLX-TURX) : Math.abs(TLLY-TURY); //Calculate birdge length from coordinates
+		bridgeTileLength = (Math.abs(TLL[0]-TUR[0]) > Math.abs(TLL[1]-TUR[1])) ? Math.abs(TLL[0]-TUR[0]) : Math.abs(TLL[1]-TUR[1]); //Calculate birdge length from coordinates
 		
 	}
 	
@@ -35,7 +42,7 @@ public class Navigator {
 		int tCornerIntX = 0, tCornerIntY = 0;
 		boolean turnFirLoc = true, turnSecLoc = true;
 		if(SC == 3) {
-			if(TURY == 9) {
+			if(TUR[1] == 9) {
 				tCornerIntX = -1;
 				tCornerIntY = 0;
 				turnFirLoc = false;
@@ -49,7 +56,7 @@ public class Navigator {
 			
 		}
 		else {
-			if(TURX == 15) {
+			if(TUR[0] == 15) {
 				tCornerIntX = 0;
 				tCornerIntY = -1;
 				turnFirLoc = true;
@@ -61,17 +68,17 @@ public class Navigator {
 				turnSecLoc = true;
 			}
 		}
-		move.travelTo((TLLX + tCornerIntX)*TILE_SIZE, (TLLY + tCornerIntY)*TILE_SIZE, false); //Travel to the tile intersection diagonally opposite to LL tunnel corner
-		move.turnTo(90); 
+		movementController.travelTo((TLL[0] + tCornerIntX)*TILE_SIZE, (TLL[1] + tCornerIntY)*TILE_SIZE, false); //Travel to the tile intersection diagonally opposite to LL tunnel corner
+		movementController.turnTo(90); 
 		
 		localizer.quickLocalization(); //Correction in y
-		move.driveDistance(-VERT_SENSOR_OFFSET); 
-		move.rotateAngle(90, turnFirLoc);
-		move.driveDistance(-5);
+		movementController.driveDistance(-VERT_SENSOR_OFFSET); 
+		movementController.rotateAngle(90, turnFirLoc);
+		movementController.driveDistance(-5);
 		
 		localizer.quickLocalization(); //Correction in x
-		move.driveDistance((TILE_SIZE/2)-VERT_SENSOR_OFFSET); //Position itself in the middle of the tile in front and facing the tunnel
-		move.rotateAngle(90, turnSecLoc);
+		movementController.driveDistance((TILE_SIZE/2)-VERT_SENSOR_OFFSET); //Position itself in the middle of the tile in front and facing the tunnel
+		movementController.rotateAngle(90, turnSecLoc);
 		
 	}
 	
@@ -83,7 +90,7 @@ public class Navigator {
 		int thetaCor = 0;
 		boolean turnLoc = true;
 		if(SC == 3) {
-			if(TURY == 9) {
+			if(TUR[1] == 9) {
 				posCorX = 1;
 				posCorY = -1;
 				turnLoc = true;
@@ -103,16 +110,16 @@ public class Navigator {
 				thetaCor = 0;
 		}
 		localizer.quickLocalization(); //Make sure robot is straight
-		move.driveDistance((bridgeTileLength+2)*TILE_SIZE - VERT_SENSOR_OFFSET); //Cross tunnel
+		movementController.driveDistance((bridgeTileLength+2)*TILE_SIZE - VERT_SENSOR_OFFSET); //Cross tunnel
 		
 		localizer.quickLocalization(); //Correct angle and x position 
-		move.driveDistance(-VERT_SENSOR_OFFSET); 
-		move.rotateAngle(90, turnLoc);
+		movementController.driveDistance(-VERT_SENSOR_OFFSET); 
+		movementController.rotateAngle(90, turnLoc);
 		
 		localizer.quickLocalization(); //Correct y position
-		move.driveDistance(-VERT_SENSOR_OFFSET);
+		movementController.driveDistance(-VERT_SENSOR_OFFSET);
 		
-		odo.setXYT((TURX + posCorX)*TILE_SIZE, (TURY + posCorY)*TILE_SIZE, thetaCor);
+		odo.setXYT((TUR[0] + posCorX)*TILE_SIZE, (TUR[1] + posCorY)*TILE_SIZE, thetaCor);
 	}
 	
 	/**
@@ -120,15 +127,18 @@ public class Navigator {
 	 */
 	public void travelToBTunnel() {
 		
-		move.travelTo((TURX+1)*TILE_SIZE, odo.getXYT()[1], false);
+		movementController.travelTo((TUR[0]+1)*TILE_SIZE, odo.getXYT()[1], false);
 		localizer.quickLocalization();
-		move.driveDistance(-VERT_SENSOR_OFFSET);
+		movementController.driveDistance(-VERT_SENSOR_OFFSET);
 		odo.setTheta(180);
-		move.travelTo(odo.getXYT()[0], TURY*TILE_SIZE, false);
+		movementController.travelTo(odo.getXYT()[0], TUR[1]*TILE_SIZE, false);
 		
 	}
 	
 	public void goToStartingTile() {
-		if(St)
+		if(SC==0) {
+			
+			
+		}
 	}
 }
