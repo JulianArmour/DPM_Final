a6d5797a8c4bf8506405acc37dd1f2a26d0d3745
Julian Armour <julian.armour@videotron.ca>
2019-03-05 16:37:20 -0500

    wrote methods for grabbing and releasing cans. Closes #22, #23.

--- src/ca/mcgill/ecse211/arms/Claw.java
+++ src/ca/mcgill/ecse211/arms/Claw.java
@@ -1,5 +1,56 @@
 package ca.mcgill.ecse211.arms;
 
-public class Claw {
+import java.rmi.RemoteException;
 
+import lejos.remote.ev3.RMIRegulatedMotor;
+
+/**
+ * Provides methods for controlling the motor that will grab the cans.
+ * 
+ * @author Julian Armour
+ * @version 1
+ * @since March 5, 2019
+ */
+public class Claw {
+    private RMIRegulatedMotor claw;
+    private static int RELEASED_ANGLE = 15;// TODO
+    private static int GRABBED_ANGLE = 0; //TODO
+    
+    /**
+     * 
+     * @param clawMotor the motor used for the claw that grabs the cans
+     * @author Julian Armour
+     */
+    public Claw(RMIRegulatedMotor clawMotor) {
+        this.claw = clawMotor;
+    }
+    
+    /**
+     * Causes the claw arm to close around a can.
+     * 
+     * @author Julian Armour
+     * @since March 5, 2019
+     */
+    public void grabCan() {
+        try {
+            claw.rotateTo(GRABBED_ANGLE, false);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    /**
+     * Releases a can that's held in the claw.
+     * 
+     * @author Julian Armour
+     * @since March 5, 2019
+     */
+    public void releaseCan() {
+        try {
+            claw.rotateTo(RELEASED_ANGLE, false);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+    
 }
