be592ca18be2d2ec4b88126ea95f5e50eada3b74
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2019-03-08 13:08:27 -0500

    Implemented method for determining of the can is heavy. Closes #21.

--- src/ca/mcgill/ecse211/detectors/WeightDetector.java
+++ src/ca/mcgill/ecse211/detectors/WeightDetector.java
@@ -1,10 +1,34 @@
 package ca.mcgill.ecse211.detectors;
 
+import java.rmi.RemoteException;
+
 import lejos.remote.ev3.RMISampleProvider;
 
 public class WeightDetector {
 
     private RMISampleProvider touchSampler;
     
+    public WeightDetector(RMISampleProvider rmiSampleProvider) {
+        this.touchSampler = rmiSampleProvider;
+    }
+    
+    /**
+     * @return true the can is heavy
+     * 
+     * @author Julian Armour
+     * @since March 8 2019
+     */
+    public boolean isCanHeavy() {
+        try {
+            if (touchSampler.fetchSample()[0] == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
     
 }
