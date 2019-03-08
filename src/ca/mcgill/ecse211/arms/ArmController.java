f50874bfbbed8d2059cf92f1b224850c037bcf91
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2019-03-08 17:35:41 -0500

    implemented positionArmToGrabCan(). Closes #36

--- src/ca/mcgill/ecse211/arms/ArmController.java
+++ src/ca/mcgill/ecse211/arms/ArmController.java
@@ -1,5 +1,13 @@
 package ca.mcgill.ecse211.arms;
 
+/**
+ * Provides the methods for all needed arm movement routines.
+ * 
+ * @author Julian Armour
+ * @since March 8 2019
+ * @version 1
+ *
+ */
 public class ArmController {
     
     private Claw claw;
@@ -9,4 +17,19 @@ public class ArmController {
         this.claw = claw;
         this.elbow = elbow;
     }
+    
+    /**
+     * Positions the arm in such a way that it is ready to grab a can in front of the robot.
+     * 
+     * @author Julian Armour
+     * @since March 8 2019
+     */
+    public void positionArmToGrabCan() {
+        claw.releaseCan(); // open the claw
+        elbow.lowerArmToFloor();
+    }
+    
+    
+    
+    
 }
