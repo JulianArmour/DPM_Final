d736bb02359fa7b472098db54fd156adbd94132e
JulianArmour <35701671+JulianArmour@users.noreply.github.com>
2019-03-08 17:46:10 -0500

    Changed method do grab a can on the floor so #36 remains consistent

--- src/ca/mcgill/ecse211/arms/ArmController.java
+++ src/ca/mcgill/ecse211/arms/ArmController.java
@@ -9,27 +9,31 @@ package ca.mcgill.ecse211.arms;
  *
  */
 public class ArmController {
-    
-    private Claw claw;
+
+    private Claw  claw;
     private Elbow elbow;
 
     public ArmController(Claw claw, Elbow elbow) {
         this.claw = claw;
         this.elbow = elbow;
     }
-    
+
     /**
-     * Positions the arm in such a way that it is ready to grab a can in front of the robot.
+     * Positions the arm in such a way that it is ready to grab a can in front of
+     * the robot. This method assumes that the can is properly located in front of
+     * the robot so that the arm movements will work.
      * 
      * @author Julian Armour
      * @since March 8 2019
      */
-    public void positionArmToGrabCan() {
+    public void grabCanOnFloor() {
         claw.releaseCan(); // open the claw
         elbow.lowerArmToFloor();
+        claw.grabCan();
     }
-    
-    
-    
-    
+
+    public void dropCanInBasket() {
+
+    }
+
 }
