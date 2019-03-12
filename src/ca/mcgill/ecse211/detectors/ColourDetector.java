9b57327ebf5e7089fc2e6da500405d0b9154866c
Julian Armour <julian.armour@videotron.ca>
2019-03-12 10:45:14 -0400

    Implemented #19

--- src/ca/mcgill/ecse211/detectors/ColourDetector.java
+++ src/ca/mcgill/ecse211/detectors/ColourDetector.java
@@ -1,8 +1,12 @@
 package ca.mcgill.ecse211.detectors;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import ca.mcgill.ecse211.arms.ArmController;
+import lejos.robotics.SampleProvider;
+import lejos.utility.Timer;
+import lejos.utility.TimerListener;
 
 /**
  * Contains methods for collecting colour data from cans and determining the
@@ -12,28 +16,64 @@ import ca.mcgill.ecse211.arms.ArmController;
  * @version 1
  * @since March 8 2019
  */
-public class ColourDetector {
-    
-    private ArmController armController;
+public class ColourDetector implements TimerListener {
 
-    public ColourDetector(ArmController armController) {
+    private static final int COLOUR_POLL_PERIOD = 50;
+    private ArmController    armController;
+    private SampleProvider   colourSampler;
+
+    private List<float[]>    colourSamples;
+    private Timer            colourPoller;
+
+    public ColourDetector(ArmController armController, SampleProvider colourSampler) {
         this.armController = armController;
+        this.colourSampler = colourSampler;
+        this.colourPoller = new Timer(COLOUR_POLL_PERIOD, this);
     }
 
     /**
      * This is the main entry point for starting the colour detection routine.
      * <p>
      * It should be called after {@link ArmController#grabCanOnFloor()}
+     * <p>
+     * The colour data can then be retrieved with {@link #getColourSamples()}
      * 
-     * @return a list of colour detected from the can
+     * @param numberOfScans
+     *            The can will be scanned 2 x numberOfScans.
      * 
      * @author Julian Armour
      * @since March 8, 2019
      */
-    public List<float[]> collectColourData() {
-        // TODO implement this. This method uses the ArmController class.
+    public void collectColourData(int numberOfScans) {
         armController.moveArmToScanningPosition();
-        // TODO start scanning routine
-        return null;
+        // initialize a new list
+        colourSamples = new LinkedList<float[]>();
+        // start polling colour data
+        colourPoller.start();
+        // start arm scan movement routine
+        armController.performScanMovement(numberOfScans);
+        // stop polling colour data
+        colourPoller.stop();
+    }
+
+    /**
+     * 
+     * @return The colour samples from the previous scan
+     */
+    public List<float[]> getColourSamples() {
+        return colourSamples;
+    }
+
+    /**
+     * Fetches a colour sample and adds it to a list.
+     * 
+     * @author Julian Armour
+     * @since March 12, 2019
+     */
+    @Override
+    public void timedOut() {
+        float[] sample = new float[colourSampler.sampleSize()];
+        colourSampler.fetchSample(sample, 0);
+        colourSamples.add(sample);
     }
 }
