c002b2289b37de9e5e83c96e462525362b2db2d5
Julian Armour <julian.armour@videotron.ca>
2019-03-14 10:54:06 -0400

    claw speed as constant instead of contructor param

--- src/ca/mcgill/ecse211/arms/Claw.java
+++ src/ca/mcgill/ecse211/arms/Claw.java
@@ -8,13 +8,13 @@ import lejos.hardware.motor.NXTRegulatedMotor;
  * Provides methods for controlling the motor that will grab the cans.
  * 
  * @author Julian Armour
- * @version 2
- * @since March 13, 2019
+ * @version 3
+ * @since March 14, 2019
  */
 public class Claw {
-    
+
     private EV3LargeRegulatedMotor claw;
-    private int                    clawSpeed;
+    private static final int       CLAW_SPEED = 360;
     private static int             RELEASED_ANGLE = 0;  // TODO
     private static int             GRABBED_ANGLE  = 45; // TODO
 
@@ -23,9 +23,8 @@ public class Claw {
      *            the motor used for the claw that grabs the cans
      * @author Julian Armour
      */
-    public Claw(EV3LargeRegulatedMotor clawMotor, int clawSpeed) {
+    public Claw(EV3LargeRegulatedMotor clawMotor) {
         this.claw = clawMotor;
-        this.clawSpeed = clawSpeed;
     }
 
     /**
@@ -35,8 +34,8 @@ public class Claw {
      * @since March 5, 2019
      */
     public void grabCan() {
-            claw.setSpeed(clawSpeed);
-            claw.rotateTo(GRABBED_ANGLE, false);
+        claw.setSpeed(CLAW_SPEED);
+        claw.rotateTo(GRABBED_ANGLE, false);
     }
 
     /**
@@ -46,8 +45,8 @@ public class Claw {
      * @since March 5, 2019
      */
     public void releaseCan() {
-            claw.setSpeed(clawSpeed);
-            claw.rotateTo(RELEASED_ANGLE, false);
+        claw.setSpeed(CLAW_SPEED);
+        claw.rotateTo(RELEASED_ANGLE, false);
     }
 
 }
