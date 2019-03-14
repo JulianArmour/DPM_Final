0993b4d96bf17ef9dd3ee667f7427ff3e849b20a
Julian Armour <julian.armour@videotron.ca>
2019-03-14 10:40:55 -0400

    updated #21 for hardware V3. Closes #21

--- src/ca/mcgill/ecse211/detectors/WeightDetector.java
+++ src/ca/mcgill/ecse211/detectors/WeightDetector.java
@@ -11,23 +11,43 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
  * @version 2
  */
 public class WeightDetector {
+    
+    private static final int HEAVY_CAN_THRESHOLD = 30;// TODO
+    private static final int DETECT_SPEED = 1000;
+    private static final int DETECT_ACC = 10000;
 
     private EV3LargeRegulatedMotor clawMotor;
     private MovementController     movementController;
 
-    public WeightDetector(EV3LargeRegulatedMotor clawMotor, MovementController movementController) {
+    private int tileLength;
+
+    public WeightDetector(EV3LargeRegulatedMotor clawMotor, MovementController movementController, int tileLength) {
         this.clawMotor = clawMotor;
         this.movementController = movementController;
+        this.tileLength = tileLength;
     }
 
     /**
+     * Jerks the robot back which causes the claw to open up past a certain
+     * threshold when holding a heavy can. The robot will return to it's original
+     * position after this motion.
+     * 
      * @return true if the can is heavy
      * 
      * @author Julian Armour
-     * @since March 8 2019
+     * @since March 14 2019
      */
     public boolean isCanHeavy() {
-        // TODO
+        int initTacho = clawMotor.getTachoCount();
+        clawMotor.flt();
+        movementController.driveDistance(tileLength/2, DETECT_SPEED, DETECT_ACC, false);
+        int dTacho = Math.abs(clawMotor.getTachoCount() - initTacho);
+        movementController.driveDistance(tileLength/2, false);
+        if (dTacho > HEAVY_CAN_THRESHOLD) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
